4203addc780dcea55000b9882a568714bd20b825
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-08 12:17:38 +0530

    page rendered but clicking + doesn't work. Fixing next

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,17 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( jquery-ui.min.css )
+Rails.application.config.assets.precompile += %w( material.min.css )
+Rails.application.config.assets.precompile += %w( mdDateTimePicker.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( jquery-3.0.0.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
+Rails.application.config.assets.precompile += %w( material.min.js )
+Rails.application.config.assets.precompile += %w( moment.min.js )
+Rails.application.config.assets.precompile += %w( draggabilly.pkgd.min.js )
+Rails.application.config.assets.precompile += %w( mdDateTimePicker.js )
+Rails.application.config.assets.precompile += %w( app.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
